3edc1b1bd6609cdda344d9bcfc66c11783b22fc7
AbstractAvival <chrisFJ.avival@gmail.com>
2025-06-14 19:06:07 -0600

    Created Enumeration classes 'GameStates' and 'GameTransitions'. Added MainMenuState and PlayState to GameState Enumeration class. I'm leaving GameTransitions empty for now.

---
+++ Monogame Game Mechanics/General/Enums/GameStates.cs
@@ -0,0 +1,17 @@
+﻿/********************************************
+Programmer: Avival
+Date: June 16, 2025
+
+Class Description: 
+Enumeration class that serves as a list of all the game's possible states. 
+Game states stored in the game's Statehandler MUST be initialized according to
+the order in this file!
+********************************************/
+namespace Monogame_Game_Mechanics.General.Enums
+{
+	public enum GameStates
+	{
+		MainMenuState,
+		PlayState
+	}
+}
---
+++ Monogame Game Mechanics/General/Enums/GameTransitions.cs
@@ -0,0 +1,16 @@
+﻿/********************************************
+Programmer: Avival
+Date: June 16, 2025
+
+Class Description: 
+Enumeration class that serves as a list of all the game's possible transitions. 
+Transitions stored in the game's Transitionhandler MUST be initialized according to
+the order in this file!
+********************************************/
+namespace Monogame_Game_Mechanics.General.Enums
+{
+	public enum GameTransitions
+	{
+
+	}
+}
