0271a96125e1649fb80ed806cd39d37f3db6956a
AbstractAvival <chrisFJ.avival@gmail.com>
2025-06-14 20:20:08 -0600

    Implemented skeleton ResolutionHandler class and AspectRatio enumeration. Will be used in the future to change the game's resolution and decide optimal asset sizes.

---
+++ Monogame Game Mechanics/General/Enums/AspectRatios.cs
@@ -0,0 +1,16 @@
+﻿/********************************************
+Programmer: Avival
+Date: June 16, 2025
+
+Class Description: 
+Enumeration class that serves as a list of all the game's supported aspect 
+ratios. Used by ResolutionHandler class to identify the optimal asset 
+resolutions.
+********************************************/
+namespace Monogame_Game_Mechanics.General.Enums
+{
+	public enum AspectRatios
+	{
+
+	}
+}
---
+++ Monogame Game Mechanics/Handlers/ResolutionHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_Game_Mechanics.Handlers
+{
+	public class ResolutionHandler
+	{
+		public ResolutionHandler( GraphicsDeviceManager graphicsIn ) 
+		{
+			graphics = graphicsIn;
+		}
+
+		private GraphicsDeviceManager graphics;
+	}
+}
